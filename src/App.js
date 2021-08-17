b05079ff0360b8dd0d6ce5b8cd2cefd3f72254e7
qdchongMom <chong_qi_da@mom.gov.sg>
2021-08-17 10:22:15 +0800

    updated

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import TodoList from "./components/TodoList";
+//import TodoList from "./components/TodoList";
 import TodoNew from "./components/TodoNew";
 
 function App() {
